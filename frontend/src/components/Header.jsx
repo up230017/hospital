25714ace271701306429d6044301c1b0ab522ebf
David Duarte Delgado <up230798@alumnos.upa.edu.mx>
2025-07-14 21:37:17 -0600

    Corrección Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col md:flex-row flex-wrap bg-jungle-green rounded-lg px-6 md:px-10 lg:px-20'>
+
+
+        {/* Left Side */}
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+            <p className='text-3xl md:text4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
+                ¡Agenda una cita <br /> con los mejores Médicos!
+            </p>
+            <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+                <img className='w-28' src={assets.group_profiles} alt="" />
+                <p>Solo vea nuestra lista de profesionales <br className='hidden sm:block' /> y agende una cita</p>
+            </div>
+            <a className='flex items-canter gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300' href="#speciality">
+                Agendar Cita <img className='w-3' src={assets.arrow_icon} alt="" />
+            </a>
+        </div>
+
+        {/* Right Side */}
+        <div className='md:w-1/2 relative'>
+            <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
