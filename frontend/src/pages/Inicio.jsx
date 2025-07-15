16bc761bd4e95f684ffadcb0da3b69ff5722f8c7
David Duarte Delgado <up230798@alumnos.upa.edu.mx>
2025-07-14 21:40:25 -0600

    corrección especialidades

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 const Inicio = () => {
   return (
     <div>
       <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
