a82e256531d5341f4bca8b839a90af175c8552a1
up230017 <up230017@alumnos.upa.edu.mx>
2025-07-16 17:05:15 -0600

    feat (cita) : :sparkles: se crearon las rutas de las citas

--- js/API/entities/indexRoutes.js
+++ js/API/entities/indexRoutes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const usuraioRoutes = require('./usuario/usuarioRoutes');
+const citaRoutes = require('./cita/citaRoutes');
 
 router.use(usuraioRoutes);
+router.use(citaRoutes);
 
 module.exports = router;
