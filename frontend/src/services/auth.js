df74c1e54bec1475708fe81da532a40b0471c33a
David Duarte Delgado <up230798@alumnos.upa.edu.mx>
2025-07-16 18:26:45 -0600

    auth.js

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,31 @@
+import api from '../config/services';
+
+export const login = async (email, password) => {
+  try {
+    const response = await api.post('/auth/login', { email, password });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const register = async (userName, email, password) => {
+  try {
+    const response = await api.post('/auth/register', { userName, email, password });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export const refreshToken = async (token) => {
+  try {
+    const response = await api.post('/auth/refresh-token', { token });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+refreshToken
